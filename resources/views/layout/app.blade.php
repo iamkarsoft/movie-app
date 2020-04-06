a0fef4b4927dcfc096e144484be6c495500d3ea8
Kofi Ramos Amoussou <karsoft92@gmail.com>
2020-04-06 10:28:14 +0000

    init

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Movie App</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link rel="stylesheet" href="/css/main.css">
+    <!-- Styles -->
+</head>
+<body class="font-sans bg-gray-900 text-white">
+    <nav class="border-b border-gray-800">
+        <div class="container mx-auto flex justify-between px-4 py-6">
+            <ul class="flex items-center">
+                <li><a href="">Movies</a></li>
+                <li><a href="">TV Shows</a></li>
+                <li><a href="">Actors</a></li>
+            </ul>
+            <div class="flex items-center">
+                <div class="relative">
+                    <input type="text" class="bg-gray-800 px-5 w-65 pl-8 py-1 rounded-full focus:outline-none focus:shadow-outline" placeholder="search">
+                </div>
+
+            </div>
+        </div>
+    </nav>
+        @yield('content')
+
+</body>
+</html>
