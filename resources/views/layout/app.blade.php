1ba611bb8aff58f2cd2c723e9ffcf09f4875806a
Kofi Ramos Amoussou <karsoft92@gmail.com>
2021-02-18 13:07:17 +0000

    footer and credit TMDB API

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -37,6 +37,11 @@
         </div>
     </nav>
         @yield('content')
+
+<footer class="border border-t border-gray-800 mt-4">
+        <div class="container mx-auto text-sm px-4 py-6">
+            Powered by <a href="https://www.themoviedb.org/documentation/api" class="underline hover:text-gray-300">TMDb API</a>
+        </div>
 <livewire:scripts>
 </body>
 </html>
