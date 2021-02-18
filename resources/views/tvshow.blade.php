bf97e37b49cf6aa1dba685be077f2e59323ede1d
Kofi Ramos Amoussou <karsoft92@gmail.com>
2021-02-18 13:28:03 +0000

    change rating icon and color

--- resources/views/tvshow.blade.php
+++ resources/views/tvshow.blade.php
@@ -11,7 +11,9 @@
 
               <div class="mt-2">
              <div class="flex items-center text-gray-400 mt-1">
-               <span><svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-6 h-6"><path d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"></path></svg></span>
+               <span>   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-6 h-6 text-red-600">
+  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+</svg></span>
                {{-- <span class="ml-1">{{ $tv['vote_average'] * 10 . '%'}}</span> --}}
                <span class="mx-2">|</span>
                <span>{{ \Carbon\Carbon::parse($tv['first_air_date'])->format('M d, Y')}}</span>
