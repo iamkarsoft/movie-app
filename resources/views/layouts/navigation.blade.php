8ad253cfaab3d03f71bd4be79d0b2ff9697bedae
Kofi Romain Amoussou <karsoft92@gmail.com>
2022-02-22 19:45:03 +0000

    add login/logout feature

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,8 +12,17 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                      <x-nav-link :href="route('movies')" >
+                        {{ __('Home') }}
+                    </x-nav-link>
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('movie.list')" >
+                        {{ __('Movies') }}
+                    </x-nav-link>
+                      <x-nav-link :href="route('tv.list')" >
+                        {{ __('Tv Shows') }}
                     </x-nav-link>
                 </div>
             </div>
