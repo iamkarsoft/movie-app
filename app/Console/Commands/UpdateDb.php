a8362c0f5f314c20933cab8c4ba2861b940714d0
Kofi Romain Amoussou <karsoft92@gmail.com>
2022-04-20 01:21:51 +0000

    commands to update and notify members

---
+++ app/Console/Commands/UpdateDb.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class UpdateDb extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'movies:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This will update the release, last aired and next air dates for all movies';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $controller = app()->make('App\Http\Controllers\UpdateMoviesController');
+        app()->call($controller);
+        return 0;
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Console\Commands\UpdateDb;
+use App\Console\Commands\NotifyMember;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -25,6 +27,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command(NotifyMember::class)->dailyAt('00:10');
+        $schedule->command(UpdateDb::class)->dailyAt('00:05');
     }
 
     /**
@@ -34,7 +38,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
