8a6a48a8c1917b9a2e9808f13e0866becf26830e
Kofi Romain Amoussou <karsoft92@gmail.com>
2022-04-25 01:59:50 +0000

    wip

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,8 @@ mix.js("resources/js/app.js", "public/js")
   .postCss("resources/css/app.css", "public/css", [
     require("tailwindcss"),
   ]);
+
+
+  mix.browserSync({
+    proxy: 'movie-app.test'
+});
