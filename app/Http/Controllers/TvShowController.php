157d2bd1847c0d6f8cf955599e991d86f774ae44
Kofi Romain Amoussou <karsoft92@gmail.com>
2022-05-13 15:48:01 +0000

    fix series adding and removal

--- app/Http/Controllers/TvShowController.php
+++ app/Http/Controllers/TvShowController.php
@@ -1,50 +1,68 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Movie;
+use App\Models\MovieUser;
 use Illuminate\Support\Facades\Http;
 
-class TvShowController extends Controller {
-	//
-	public function index() {
+class TvShowController extends Controller
+{
+    //
+    public function index()
+    {
 
-		// tv shows
+        // tv shows
 
-		$popularShow = Http::withToken(config('services.tmdb.token'))
-			->get('https://api.themoviedb.org/3/tv/popular')
-			->json()['results'];
+        $popularShow = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/tv/popular')
+            ->json()['results'];
 
-		$popularShow = collect($popularShow)->sortBy('first_air_date')->reverse()->toArray();
+        $popularShow = collect($popularShow)->sortBy('first_air_date')->reverse()->toArray();
 
-		$tvShow = Http::withToken(config('services.tmdb.token'))
-			->get('https://api.themoviedb.org/3/tv/on_the_air')
-			->json()['results'];
+        $tvShow = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/tv/on_the_air')
+            ->json()['results'];
 
-		$tvShows = collect($tvShow)->sortBy('last_episode_to_air ')->reverse()->toArray();
+        $tvShows = collect($tvShow)->sortBy('last_episode_to_air ')->reverse()->toArray();
 
-		$tvGenres = Http::withToken(config('services.tmdb.token'))
-			->get('https://api.themoviedb.org/3/genre/tv/list')
-			->json()['genres'];
+        $tvGenres = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/genre/tv/list')
+            ->json()['genres'];
 
-		return view('series', [
-			'popularShow' => $popularShow,
-			'tvShows' => $tvShows,
-			'tvGenres' => $tvGenres,
-		]);
-	}
-	public function show($id) {
-		$tv = Http::withToken(config('services.tmdb.token'))
-			->get('https://api.themoviedb.org/3/tv/' . $id . '?append_to_response=credits,videos,images,')
-			->json();
+        return view('series', [
+            'popularShow' => $popularShow,
+            'tvShows' => $tvShows,
+            'tvGenres' => $tvGenres,
+        ]);
+    }
+    public function show($id)
+    {
+        $tv = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/tv/' . $id . '?append_to_response=credits,videos,images,')
+            ->json();
 
         // dd($tv);
-          if($tv['original_name']){
+        if ($tv['original_name']) {
             $identifiable = $tv['original_name'];
-        }else{
+        } else {
             $identifiable = $tv['name'];
         }
-          $movie_db = Movie::where('name', $identifiable)->orWhere('movie_id',$tv['id'])->first();
 
-		return view('tvshow', compact('tv','movie_db'));
-	}
+        if (auth()->user()) :
+            $movie_db = MovieUser::join('movies', 'movies.id', '=', 'movie_user.movie_id')
+                ->join('users', 'users.id', '=', 'movie_user.user_id')
+                ->select('users.*', 'movies.*', 'movie_user.*')
+                ->where('movies.name',  $identifiable)
+                ->where('movie_user.user_id', auth()->user()->id)
+                ->first();
+        else :
+
+            $movie_db = Movie::query()
+                ->where('movies.name',  $identifiable);
+
+        endif;
+
+        return view('tvshow', compact('tv', 'movie_db'));
+    }
 }
