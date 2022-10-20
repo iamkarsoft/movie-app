24669254680e9c511c71b0ec8115a01249cf5a04
kofi amoussou <karsoft92@gmail.com>
2022-10-20 08:53:49 +0000

    add htacces

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,13 @@ set -e
 echo "Deploying application ..."
 
 # Enter maintenance mode
-(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+# (php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
     # Update codebase
     git fetch origin master
     git reset --hard origin/master
 
     # Install dependencies based on lock file
-    composer install --no-interaction --prefer-dist --optimize-autoloader
+    # composer install --no-interaction --prefer-dist --optimize-autoloader
 
     # Migrate database
     # php artisan migrate --force
@@ -24,6 +24,6 @@ echo "Deploying application ..."
     # Reload PHP to update opcache
     #echo "" | sudo -S service php7.4-fpm reload
 # Exit maintenance mode
-php artisan up
+# php artisan up
 
 echo "Application deployed!"
