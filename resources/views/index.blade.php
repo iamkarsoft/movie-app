b127a43a5ed09f98653bfe056c71ba04ca338bec
kofi amoussou <karsoft92@gmail.com>
2022-10-21 13:50:09 +0000

    wip

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
  <div class="container  mx-auto px-4 pt-16 pb-16">
     <div class="popular-movies mt-8 w-full lg:mx-auto">
       <h2 class=" text-4xl tracking-wider text-orange-500 font-semibold uppercase text-lg">
-        Popular Movies Kofi Ramos
+        Popular Movies Right Now
       </h2>
 
       <div class="md:grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
