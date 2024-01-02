201267486887e853c0454b4461e771dcebbf1474
kofi amoussou <karsoft92@gmail.com>
2024-01-02 23:27:36 +0000

    Add 2nd view option with fixing fillable

--- resources/views/components/movie-card.blade.php
+++ resources/views/components/movie-card.blade.php
@@ -32,7 +32,7 @@
                   <span>{{ \Carbon\Carbon::parse($movie['release_date'])->format('M d, Y')}}</span>
 
            </div>
-             <div class="text-gray-400 text-sm flex gap-2 my-2">
+             <div class="text-gray-400 text-sm gap-2 my-2">
                  <a href="https://lookmovie2.to/movies/search/?q={{$searchTitle}}" target="_blank" class="flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-red-600">
   <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
@@ -41,6 +41,14 @@
                      <span>Viewing Option 1</span>
 
                      </a>
+
+                  <a href="https://sflix.to/search/{{\Str::kebab($movie['title'])}}" target="_blank" class="flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-red-600">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                </svg>
+                <span>Viewing Option 2</span>
+            </a>
              </div>
          </div>
       </a>
--- resources/views/tvshow.blade.php
+++ resources/views/tvshow.blade.php
@@ -53,6 +53,27 @@
                         @endauth
                 </div>
 
+                <div>
+                     <div class="text-gray-400 text-sm flex gap-2 my-2">
+                            <a href="https://lookmovie2.to/movies/search/?q={{$tv['name']}}" target="_blank" class="flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-red-600">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                </svg>
+                                <span>Viewing Option 1</span>
+                            </a>&nbsp;
+
+                        <a href="https://sflix.to/search/{{\Str::kebab($tv['name'])}}" target="_blank" class="flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-red-600">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                        </svg>
+                        <span>Viewing Option 2</span>
+                        </a>
+                     </div>
+
+                </div>
+
 
                 <div class="mt-12">
                     <h4 class="font-semibold text-white">Cast</h4>
