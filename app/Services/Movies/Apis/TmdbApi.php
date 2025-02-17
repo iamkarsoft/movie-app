76844807374faa04f13927dd2e3f08b9350a7871
iamkarsoft <5146869+iamkarsoft@users.noreply.github.com>
2025-02-17 23:05:03 +0000

    Apply php-cs-fixer changes

--- app/Services/Movies/Apis/TmdbApi.php
+++ app/Services/Movies/Apis/TmdbApi.php
@@ -2,20 +2,20 @@
 
 namespace App\Services\Movies\Apis;
 
-    use App\Contracts\MovieApiConnection;
-    use Http;
+use App\Contracts\MovieApiConnection;
+use Http;
 
-    class TmdbApi implements MovieApiConnection
+class TmdbApi implements MovieApiConnection
+{
+    public static function connect(string $token, string $url, string $type)
     {
-        public static function connect(string $token, string $url, string $type)
-        {
-            return Http::withToken(config($token))->get($url)
-            ->json()[$type];
-        }
-
-        public static function getGenres()
-        {
-            return Http::withToken(config('services.tmdb.token'))->get('https://api.themoviedb.org/3/genre/movie/list')
-            ->json()['genres'];
-        }
+        return Http::withToken(config($token))->get($url)
+        ->json()[$type];
     }
+
+    public static function getGenres()
+    {
+        return Http::withToken(config('services.tmdb.token'))->get('https://api.themoviedb.org/3/genre/movie/list')
+        ->json()['genres'];
+    }
+}
